9170a3221c2c36a0c28b8de98f724ae2413d034a
Nicolas Posa <nicoposa57@gmail.com>
2021-07-02 16:05:11 -0300

    add typescript and the avo api from our our db

---
+++ pages/about/about.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar/Navbar";
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>about</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/api/avo/index.ts
@@ -0,0 +1,16 @@
+import { IncomingMessage, ServerResponse } from "http";
+
+import DB from "@database";
+
+const allAvos = async (request: IncomingMessage, response: ServerResponse) => {
+  const db = new DB();
+  const allEntries = await db.getAll();
+  const lenght = allEntries.length;
+
+  response.statusCode = 200;
+  response.setHeader("Content-Type", "application/json");
+
+  response.end(JSON.stringify({ data: allEntries, lenght }));
+};
+
+export default allAvos;
